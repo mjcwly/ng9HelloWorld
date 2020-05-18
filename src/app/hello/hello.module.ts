74c5a3960a6e93920c2261ece233dcfa921ab297
Matthew Cawley <mjcwly@gmail.com>
2020-05-18 05:35:19 +0100

    generated-shared-module

--- src/app/hello/hello.module.ts
+++ src/app/hello/hello.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HelloRoutingModule } from './hello-routing.module';
 import { HelloComponent } from './hello.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [HelloComponent],
   imports: [
     CommonModule,
-    HelloRoutingModule
+    HelloRoutingModule,
+    SharedModule
   ]
 })
 export class HelloModule { }
