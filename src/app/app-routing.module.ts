027937ed239d07fa204a8ffba27b84b423bcfa06
Matthew Cawley <mjcwly@gmail.com>
2020-05-18 06:04:16 +0100

    manually-configured-a-route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorldComponent } from './world/world.component';
 
-
-const routes: Routes = [{ path: 'hello-route', loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule) }];
+const routes: Routes = [
+  { path: 'hello-route', loadChildren: () => import('./hello/hello.module').then(m => m.HelloModule)  },
+  { path: 'world-route', component: WorldComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
