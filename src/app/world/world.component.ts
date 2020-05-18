4c0434c1b23d8295e3fcfeec27f6f6ab4319f3e1
Matthew Cawley <mjcwly@gmail.com>
2020-05-18 05:48:33 +0100

    generated-world-component

---
+++ src/app/world/world.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-world',
+  templateUrl: './world.component.html',
+  styleUrls: ['./world.component.less']
+})
+export class WorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
